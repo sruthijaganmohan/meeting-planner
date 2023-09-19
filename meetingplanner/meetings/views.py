f1036228636c05fcfaf7382dd76fe30ea5a2c0f4
sruthijaganmohan <sruthi12jagan@gmail.com>
2023-09-19 20:15:20 +0530

    MVT pattern

--- meetingplanner/meetings/views.py
+++ meetingplanner/meetings/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Meetings
 
 # Create your views here.
+def detail(request, id):
+    meeting = get_object_or_404(Meetings, pk=id)
+    return render(request, 'meetings/detail.html', {'meeting' : meeting})
